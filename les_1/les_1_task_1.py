b925f9ee898a11f71f22bd4f5599e4ed6e77da16
Vera Koberman <50861303+KOberman7@users.noreply.github.com>
2019-12-11 17:18:02 +0500

    Python hw lesson 1

---
+++ les_1/les_1_task_1.py
@@ -0,0 +1,12 @@
+a = 5
+b = 16.2
+
+print(a, b)
+
+num1 = int(input('Введите целое число: '))
+num2 = float(input('Введите число: '))
+
+str1 = input('Введите своё имя: ')
+
+print(num1, num2)
+print(f'Привет, {str1}!')
---
+++ les_1/les_1_task_3.py
@@ -0,0 +1,8 @@
+n = input('Введите число: ')
+
+a = n
+b = n+n
+c = n+n+n
+A = int(a) + int(b) + int(c)
+
+print(A) 
